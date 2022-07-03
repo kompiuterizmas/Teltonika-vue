f6a2f45c9a879f823e28d09472e65c3976399105
kompiuterizmas <gintautas.burmistrovas@gmail.com>
2022-07-03 22:58:08 +0300

    some edit to the user form
    i suspect it is not correct uproach, but...

--- src/views/AddUserForm.vue
+++ src/views/AddUserForm.vue
@@ -3,25 +3,30 @@
 <div><h1>Add new user</h1></div>
   <form @submit.prevent="" id="userform" action="">
     <label for="firstname">enter new user first name:</label>
-    <input type="text" name="firstname" value="" placeholder="first name"/>
+    <input type="text" name="firstname" v-model="firstname" placeholder="first name"/>
     <label for="lastname">enter new user last name:</label>
-    <input type="text" name="lastname" value="" placeholder="last name"/>
+    <input type="text" name="lastname" v-model="lastname" placeholder="last name"/>
     <label for="email">enter new user email address:</label>
-    <input type="email" name="email" value="" placeholder="e-mail"/>
+    <input type="email" name="email" v-model="email" placeholder="e-mail"/>
     <label for="password">create new user password:</label>
-    <input type="password" name="password" value="" placeholder="password"/>
+    <input type="password" name="password" v-model="password" placeholder="password"/>
     <label for="birthdate">select new user birth date:</label>
-    <input type="date" name="birthdate" value=""/>
+    <input type="date" name="birthdate" v-model="birthdate"/>
     <label for="gender">select new user gender:</label>
-    <select name="gender" id="gender" form="gender">
-    <option value="" selected="true" disabled="disabled">select gender</option>
+    <select name="gender" id="gender" form="gender" v-model="gender">
+    <option selected="true" disabled="disabled">select gender</option>
     <option value="male">male</option>
     <option value="female">female</option>
     </select>
     <label for="category">select new user category:</label>
-    <select name="category" id="category">
-    <option v-for="category in $store.state.categories" value="{{category.id}}" :key="category.id">{{category.title}}</option>
-    <!-- <option v-if="category.subcat" value=""></option> -->
+    <select name="category" id="category" v-model="category">
+    <option v-for="category in $store.state.categories" :value="category.id" :key="category.id">{{category.title}}</option>
+    </select>
+    <select name="subcat" id="subcat" v-model="subcat">
+    <option v-for="subcat in $store.state.categories.subcat" :value="subcat.id" :key="subcat.id">{{subcat.title}}</option>
+    </select>
+    <select name="subsubcat" id="subsubcat" v-model="subsubcat">
+    <option v-for="subsubcat in $store.state.subsubcat" :value="subsubcat.id" :key="subsubcat.id">{{subsubcat.title}}</option>
     </select>
     <button>Add user</button>
   </form>
